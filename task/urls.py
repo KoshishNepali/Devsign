824ec3b60df4c5ed11ed8c97f3e745f743791a78
KoshishPariyar <koshishpariyar0@gmail.com>
2026-01-16 19:56:53 +0545

    commit added

--- task/urls.py
+++ task/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns=[
     path('',views.index),
     path('second-page/',views.second_index),
+    path('third-page/',views.third_index),
 ]
--- task/views.py
+++ task/views.py
@@ -4,6 +4,22 @@ from django.http import HttpResponse
 # Create your views here.
 #function based view(ui)
 def index(request):
-    return HttpResponse("Hello we are learning django")
+    return render(request,'task/index.html',{
+        'title':'My first page',
+        'description':'This is my first page description',
+        'page_no':1
+        
+    })
+
 def second_index(request):
-    return HttpResponse("This is second page")
+    return render(request,'task/index2.html',{
+        'name':'Koshish Pariyar',
+        'course':'Python and django',
+        'time_duration':2.5
+    })
+def third_index(request):
+    return render(request,'task/index3.html',{
+        'product_name':'baggy jeans',
+        'price':3000,
+        'pant_color':"darkblue"
+    })
